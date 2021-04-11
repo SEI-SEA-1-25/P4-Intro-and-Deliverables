bb8a279bd892f33aa39fff6a4a6fc0da647f32ee
Jcom619 <joshuacompton@my.smccd.edu>
2021-04-11 02:52:13 -0700

    j

---
+++ axReqs.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+const rowdyResults = rowdy.begin(app);
+const options = {
+  method: "GET",
+  url: "https://community-open-weather-map.p.rapidapi.com/weather",
+  params: {
+    q: "London,uk",
+    lat: "0",
+    lon: "0",
+    callback: "test",
+    id: "2172797",
+    lang: "null",
+    units: '"metric" or "imperial"',
+    mode: "xml, html",
+  },
+  headers: {
+    "x-rapidapi-key": "d5780d7757msh7c61b742690e076p1596bbjsn7ad45870345d",
+    "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+  },
+};
+
+axios
+  .request(options)
+  .then(function (response) {
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    console.error(error);
+  });
---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("click", function () {
+  alert("Hello World!");
+});
+document.getElementById("p2").style.color = "blue";
