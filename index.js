4ead721b6be89a6d3f36e85c9fb344f6bd091272
Jcom619 <joshuacompton@my.smccd.edu>
2021-04-12 09:51:53 -0700

    j

--- index.js
+++ index.js
@@ -1,20 +1,53 @@
-const rowdy = "rowdy-logger";
-const express = "express";
-const axios = (PORT = "process.env.PORT");
-const rowdyResults = rowdy;
-const app = requires("express");
-
-const colorChange = () => {
-  document.getElementsByClassName("p");
+let weather = {
+  apiKey: "7a9e0d0b445ca1be17f7e626ad349281",
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apiKey
+    )
+      .then((response) => {
+        if (!response.ok) {
+          console.log("No weather found.");
+          throw new Error("No weather found.");
+        }
+        return response.json();
+      })
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".icon").src =
+      "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + "°F";
+    document.querySelector(".humidity").innerText =
+      "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText = "Wind speed: " + speed + " mph";
+    document.querySelector(".weather").classList.remove("loading");
+    // document.body.style.backgroundImage =
+    //   "url('https://source.unsplash.com/1600x900/?" + name + "')";
+  },
+  search: function () {
+    this.fetchWeather(document.querySelector(".search-bar").value);
+  },
 };
 
-const hiMundo = () => "Hello World!";
+document.querySelector(".search button").addEventListener("click", function () {
+  weather.search();
+});
 
-app.get("/", function (req, res) {
-  res.send(console("hello world"));
-});
-app.get("/weather", function (req, res) {
-  res.json(parama.colorChange.console.data());
-});
-app.listen(process.env.PORT, {});
-export default server;
+document
+  .querySelector(".search-bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+      weather.search();
+    }
+  });
+console.log(weather.fetchWeather("san francisco"));
+weather.fetchWeather("san francisco");
