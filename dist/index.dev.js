a20b5265c7af1106e2e30d883ee830a947029b4c
Jcom619 <joshuacompton@my.smccd.edu>
2021-04-15 07:27:33 -0700

    some- er- rye

--- dist/index.dev.js
+++ dist/index.dev.js
@@ -1,19 +1,16 @@
+"use strict";
+
 //?     ===========================
 //!           Date & Time
 //?     ===========================
-
 // const today = new Date();
-
 // const date =
 //   today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
-
 // const time =
 //   today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-
 //?!     ===========================
 //!         Convert Time To 12hr
 //?!     ===========================
-
 // let resultElmnt = document.querySelector(".result");
 // //  let BtnEle = document.querySelector(".Btn");
 // let hourElmnt, minuteElmnt;
@@ -33,11 +30,9 @@
 //       "TIME = " + hourElmnt + " : " + minuteElmnt + " " + amOrPm;
 //   }
 // });
-
 //?     ===========================
 //!         Get Weather Info
 //?     ===========================
-
 // let weather = {
 //   apiKey: "7a9e0d0b445ca1be17f7e626ad349281",
 //   fetchWeather: function (city) {
@@ -57,88 +52,80 @@
 //       })
 //       .then((data) => this.displayWeather(data));
 //   },
-
 //?|   ⚡︎   ⚡︎   ⚡︎   |☠︎|=====================|☠︎|   ⚡︎   ⚡︎   ⚡︎   |?\\
 //!|   ⚡︎   ⚡︎   ⚡︎   |☠︎| Show Weather & Date |☠︎|   ⚡︎   ⚡︎   ⚡︎   |?\\
 //?|   ⚡︎   ⚡︎   ⚡︎   |☠︎|=====================|☠︎|   ⚡︎   ⚡︎   ⚡︎   |?\\
-
 function displayWeather(data) {
-  const dateTime = date + " " + time;
-  console.log(today);
-  // console.log(date);
+  var dateTime = date + " " + time;
+  console.log(today); // console.log(date);
   // console.log(time);
   // console.log(dateTime);
-  const { name } = data;
-  const { icon, description } = data.weather[0];
-  const { temp, humidity } = data.main;
-  const { speed } = data.wind;
-  document.querySelector(".city").innerText =
-    "In  " + name + "\n\nOn  " + date + " / At  " + time;
-  document.querySelector(".icon").src =
-    "https://openweathermap.org/img/wn/" + icon + ".png";
+
+  var name = data.name;
+  var _data$weather$ = data.weather[0],
+      icon = _data$weather$.icon,
+      description = _data$weather$.description;
+  var _data$main = data.main,
+      temp = _data$main.temp,
+      humidity = _data$main.humidity;
+  var speed = data.wind.speed;
+  document.querySelector(".city").innerText = "In  " + name + "\n\nOn  " + date + " / At  " + time;
+  document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
   document.querySelector(".description").innerText = description;
-  let tempVal = (document.querySelector(".temp").innerText = temp + "°F");
-  // console.log(tempVal);
+  var tempVal = document.querySelector(".temp").innerText = temp + "°F"; // console.log(tempVal);
+
   document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
   document.querySelector(".wind").innerText = "Wind speed: " + speed + " mph";
   document.querySelector(".weather").classList.remove("loading");
-  document.querySelector(".today").classList.remove("☣︎", today);
-  // document.body.style.backgroundImage =
+  document.querySelector(".today").classList.remove("☣︎", today); // document.body.style.backgroundImage =
   //   "url('https://source.unsplash.com/1600x900/?" + name + "')";
 }
+
 function search() {
   this.fetchWeather(document.querySelector(".search-bar").value);
 }
+
 document.querySelector(".search button").addEventListener("click", function () {
   weather.search();
 });
-
-document
-  .querySelector(".search-bar")
-  .addEventListener("keyup", function (event) {
-    if (event.key == "Enter") {
-      weather.search();
-    }
-  });
+document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+  if (event.key == "Enter") {
+    weather.search();
+  }
+});
 console.log(weather.fetchWeather("san francisco"));
-weather.fetchWeather("san francisco");
-
-//?     ===========================
+weather.fetchWeather("san francisco"); //?     ===========================
 //!          unit conversion
 //?     ===========================
 
 function temperatureConverter(valNum) {
   valNum = parseFloat(valNum);
-  document.getElementById("outputFahrenheit").innerText =
-    (valNum - 273.15) * 1.8 + 32;
-}
-
-//?      ===========================
+  document.getElementById("outputFahrenheit").innerText = (valNum - 273.15) * 1.8 + 32;
+} //?      ===========================
 //!            style change
 //?      ===========================
 
-const changeBackground = document.getElementById("weatherUI");
+
+var changeBackground = document.getElementById("weatherUI");
 
 function switchColor(tempVal) {
-  const tempColor = document.getElementsByClassName("temp");
-  //<!--!  NEEDS DYNAMIC VARIABLE USING CONDITIONS
+  var tempColor = document.getElementsByClassName("temp"); //<!--!  NEEDS DYNAMIC VARIABLE USING CONDITIONS
   // console.log(temp);
-  let temp = parseFloat(tempVal);
-  let degree = tempColor[0].innerText;
-  const climateColorCold = "blue";
-  const climateColorWarm = "light-orange";
-  const climateColorHot = "red";
-  // console.log(degree);
+
+  var temp = parseFloat(tempVal);
+  var degree = tempColor[0].innerText;
+  var climateColorCold = "blue";
+  var climateColorWarm = "light-orange";
+  var climateColorHot = "red"; // console.log(degree);
   //<!-- if(tempColor.value ) -->
 
   console.log("Log Background Change functionality");
   changeBackground.style.backgroundColor = climateColorWarm;
-}
-
-//?      ===========================
+} //?      ===========================
 //!           This attaches the
 //!         click event listener
 //!             to the button
 //?      ===========================
+
 
 changeBackground.addEventListener("change", switchColor);
